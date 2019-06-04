62d2d0ddefc89b7f7fbc8fc578445788b8af49b8
Ellyssa Morgan <morgaell@uw.edu>
2019-06-03 21:47:52 -0700

    add bar plot for all degrees, add nav bar

--- ellyssa/final/server.R
+++ ellyssa/final/server.R
@@ -11,20 +11,29 @@ shinyServer(function(input, output) {
     selected_major <- data[[input$majors]]
     
     plot1 <- ggplot(data)+
-      geom_bar(stat = "identity", aes(x = Year , y = selected_major, fill = 'darkblue'))
+      geom_bar(stat = "identity", fill = '#b9c6fa', aes(x = Year , y = selected_major)) +
+      ylab("Percentage of Degrees") +
+      ggtitle("Degrees by Academic Specialty")
     
     return(plot1)
     
   })
   
   output$all_majors_plot <- renderPlot ({
-    major_names <- select(data, -Year)
-   # major_names <- data[1,]
-    get_degrees <- data %>% 
-        filter(Year == input$date)
+    df <- data.frame(data, row.names = data$Year)
+    df <- select(df, -Year)
+    df <- data.frame(t(df))
+    df <- mutate(df, degrees = row.names(df))
+    user_in <- paste0("X", input$date)
+    get_year <- df[[user_in]]
+    get_major <- df$degrees
     
-    plot2 <- ggplot(data) +
-        geom_bar(stat = "identity", aes(x = major_names, y = get_degrees))
+    plot2 <- ggplot(df) +
+        geom_bar(stat = "identity", fill = '#6685ff', aes(x = degrees , y = get_year))+
+        theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+        xlab("Major") +
+        ylab("Percentage of Degrees") + 
+        ggtitle("Degrees by Year")
     
     return(plot2)
   })
--- ellyssa/final/ui.R
+++ ellyssa/final/ui.R
@@ -1,12 +1,3 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
 majors <- data %>% 
@@ -17,28 +8,32 @@ major_names <- colnames(majors)
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
-  # Application title
-  titlePanel("Women's Bachelor Degree Data"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("majors", 
-                  label = "Select a Major",
-                  choices = major_names),
-      
-      numericInput("date",
-                   "Select a Year",
-                   value = 2011,
-                   min = 1970,
-                   max = 2011)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      tabsetPanel(
-        tabPanel("By Degree", plotOutput("major_plot")),
-        tabPanel("All Degrees", plotOutput("all_majors_plot"))
+  navbarPage("US Women's Bachelors Degrees",
+    tabPanel("Data By Degree",
+      sidebarLayout(
+        sidebarPanel(
+          selectInput("majors", 
+                      label = "Select a Major",
+                      choices = major_names)
+        ),
+        mainPanel(
+          plotOutput("major_plot")
+        )
       )
+    ),
+    tabPanel("All Degree Data",
+      sidebarLayout(
+        sidebarPanel( 
+          numericInput("date",
+                         "Select a Year",
+                         value = 2011,
+                         min = 1970,
+                         max = 2011)
+        ),
+        mainPanel(
+          plotOutput("all_majors_plot")
+        )
+       )
     )
   )
 ))
